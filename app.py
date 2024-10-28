07cb7a2833c8f41358d57ba08492dbb111e48e0b
gowtham <gowtham.rajendra@ontariotechu.net>
2024-10-27 21:13:15 -0400

    merged with ravi for auth

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from mongoengine import connect
 from dotenv import load_dotenv
 import os
 
+from controllers.user_controller import users_bp
+
 from models.user_model import User
 from models.question_model import Question
 
@@ -17,65 +19,75 @@ app = Flask(__name__)
 
 URL = "https://opentdb.com/api.php?"
 
-@app.route('/questions', methods=['GET'])
-def get_ques():
-    amount = request.json.get('amount')
-    type = request.json.get('type')
-    difficulty = request.json.get('difficulty')
-    category = request.json.get('category')
+# @app.route('/questions', methods=['GET'])
+# def get_ques():
+#     amount = request.json.get('amount')
+#     type = request.json.get('type')
+#     difficulty = request.json.get('difficulty')
+#     category = request.json.get('category')
 
-    response = requests.get(URL + f'amount={amount}&category={category}&difficulty={difficulty}&type={type}').json()
+#     response = requests.get(URL + f'amount={amount}&category={category}&difficulty={difficulty}&type={type}').json()
 
-    questions = []
+#     questions = []
 
-    for ques in response["results"]:
-        ques["question"] = unescape(ques["question"])
-        ques["correct_answer"] = unescape(ques["correct_answer"])
-        for i in range(len(ques["incorrect_answers"])):
-            ques["incorrect_answers"][i] = unescape(ques["incorrect_answers"][i])
+#     for ques in response["results"]:
+#         ques["question"] = unescape(ques["question"])
+#         ques["correct_answer"] = unescape(ques["correct_answer"])
+#         for i in range(len(ques["incorrect_answers"])):
+#             ques["incorrect_answers"][i] = unescape(ques["incorrect_answers"][i])
 
-        question = Question(category_name=ques["category"], 
-                     category_id=category, 
-                     difficulty=ques["difficulty"], 
-                     type=ques["type"], 
-                     question_text=ques["question"], 
-                     correct_answer=ques["correct_answer"], 
-                     incorrect_answers=ques["incorrect_answers"]
-                )
+#         question = Question(category_name=ques["category"], 
+#                      category_id=category, 
+#                      difficulty=ques["difficulty"], 
+#                      type=ques["type"], 
+#                      question_text=ques["question"], 
+#                      correct_answer=ques["correct_answer"], 
+#                      incorrect_answers=ques["incorrect_answers"]
+#                 )
         
-        question.save()
-        questions.append(question)
+#         question.save()
+#         questions.append(question)
     
-    # print(response['results'])
+#     # print(response['results'])
 
 
-    # new user example
-    # user = User(name='test2', email='test2@mail.ca', password='password', questions=questions)
-    # user.save()
+#     # new user example
+#     # user = User(name='test2', email='test2@mail.ca', password='password', questions=questions)
+#     # user.save()
 
 
-    # updating existing user example
-    user_existing = User.objects(email='test2@mail.ca').first()
+#     # updating existing user example
+#     user_existing = User.objects(email='test2@mail.ca').first()
 
-    # delete existing questions from questions collection
-    for question in user_existing.questions:
-        print('deleting question: ', question.question_text)
+#     # delete existing questions from questions collection
+#     for question in user_existing.questions:
+#         print('deleting question: ', question.question_text)
 
-        question.delete()
+#         question.delete()
 
-    user_existing.questions = questions
-    user_existing.save()
+#     user_existing.questions = questions
+#     user_existing.save()
      
 
 
-    return jsonify(response)
+#     return jsonify(response)
+
+def create_app():
+    app = Flask(__name__)
+
+    # set up config
+    app.config["JWT_SECRET"] = os.environ.get("JWT_SECRET")
+    app.config['MONGO_URI'] = DATABASE_URI
+
+    # register blueprints
+    app.register_blueprint(users_bp)
+
+    # connect to the database
+    connect('QuizAppDB', host=app.config['MONGO_URI'])
+
+    return app
 
 
 if __name__ == '__main__':
-    app.config['MONGO_URI'] = DATABASE_URI
-
-    connect('QuizAppDB', host=app.config['MONGO_URI'])
-
-    
-
+    app = create_app()
     app.run(debug=True)
---
+++ controllers/user_controller.py
@@ -0,0 +1,63 @@
+from flask import request, Blueprint
+from services.auth_service import create_jwt, token_required, hash_password, verify_password
+from models.user_model import User
+
+users_bp = Blueprint('users_bp', __name__)
+
+## TODO
+## login route
+## register route
+
+@users_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if not username or not email or not password:
+        return {"message": "Username, email and password are required."}, 401
+    
+    # email must be unique
+    if User.objects(email=email).first():
+        return {"message": "Email already registered."}, 401
+
+    new_user = User(name=username, email=email, password=hash_password(password))
+    new_user.save()
+
+    return {"message": "User created successfully."}, 201
+
+
+@users_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return {"message": "Incorrect email or password."}, 401
+
+    user = User.objects(email=email).first()
+
+    # no user with that email
+    if not user:
+        return {"message": "Incorrect email or password."}, 401
+
+    if verify_password(password, user.password):
+        access_token, refresh_token = create_jwt(user.name)
+        return {"message": "Login successful.", "access_token": access_token, "refresh_token": refresh_token}, 200
+    else:
+        return {"message": "Incorrect email or password."}, 401
+
+# @users_bp.route("/verify", methods=["POST"])
+# def verify():
+#     data = request.get_json()
+#     entered_password = data.get('entered_password')
+#     hashed_password = str.encode(data.get('hashed_password'))
+#     return {"message": "Password is correct." if verify_password(entered_password, hashed_password) else "Password is incorrect."}, 200
+
+# # testing token required decorator
+# @users_bp.route("/protected", methods=["GET"])
+# @token_required
+# def protected_route(user_data):
+#     return {"message": "This is a protected route.", "user_data": user_data}, 200
